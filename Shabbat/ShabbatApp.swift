5a7ba80d08ebdbb88e0b604db7e0b570aefb2d55
Guy Tepper <hey@guytepper.com>
2024-12-07 17:01:24 +0200

    Initial Commit

---
+++ Shabbat/ShabbatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShabbatApp.swift
+//  Shabbat
+//
+//  Created by Guy Tepper on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShabbatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
